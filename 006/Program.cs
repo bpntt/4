fb09d6235e0a0cad16ca4046b14fa88a8b98f048
BPNTT <haataya@163.com>
2022-12-15 21:32:18 +0600

    Anna

---
+++ 006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What is your name?");
+string username = Console.ReadLine(); 
+if(username == "Anna")
+{
+    Console.WriteLine("Meow...MEOW");
+}
+else 
+{
+    Console.WriteLine("Hello,");
+    Console.Write(username);
+}
