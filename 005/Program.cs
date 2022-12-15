a4f7ed26ff1ffdcf701a0d0422c132f3a34a48de
BPNTT <haataya@163.com>
2022-12-15 21:07:14 +0600

    sum

---
+++ 005/Program.cs
@@ -0,0 +1,8 @@
+﻿double numbera = new Random().Next(1,10);
+double numberb = new Random().Next(1,10);
+Console.Write(numbera);
+Console.Write("/");
+Console.Write(numberb);
+double result =numbera / numberb;
+Console.Write("=");
+Console.Write(result);
